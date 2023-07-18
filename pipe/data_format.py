4a64473808da679a1ec278d7c0b2b09c1904c7f2
sky81219 <=>
2023-07-18 14:48:39 +0900

    sytle: 주석

--- pipe/data_format.py
+++ pipe/data_format.py
@@ -3,10 +3,35 @@ from typing import Mapping, Any
 
 
 class CoinSymbol(BaseModel):
+    """
+    Subject:
+        - simpleist coin symbol\n
+    Returns:
+        - "BTC"
+    """
+
     coin_symbol: str
 
 
 class CoinMarketData(BaseModel):
+    """Coin price data schema
+    Args:
+        - BaseModel (_type_): pydantic BaseModel 으로 구현 했습니다  \n
+    Returns:
+        -  {
+            "market": "upbit-BTC",
+            "time": 1689659170616,
+            "coin_symbol": "BTC",
+            "data": {
+                "opening_price": 38761000.0,
+                "high_price": 38828000.0,
+                "low_price": 38470000.0,
+                "prev_closing_price": 38742000.0,
+                "acc_trade_volume_24h": 2754.0481778
+            }
+        }
+    """
+
     market: str
     time: int
     coin_symbol: str
