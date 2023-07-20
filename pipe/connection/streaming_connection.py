2dbc1d2ce90ada76b0f99ce35b3edfab25322ae0
sky81219 <=>
2023-07-21 01:00:23 +0900

    refactoring: streming

--- pipe/connection/streaming_connection.py
+++ pipe/connection/streaming_connection.py
@@ -80,6 +80,7 @@ def run_spark_streaming(name: str, topics: str, retrieve_topic: str) -> None:
             "value.serializer",
             "org.apache.kafka.common.serialization.ByteArraySerializer",
         )
+        .foreach(print)
         .start()
     )
 
